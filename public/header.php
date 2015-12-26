b28385749fa084fe726f423fa5b978654e8a2665
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-26 13:41:43 +0100

    painted the header

--- public/header.php
+++ public/header.php
@@ -5,8 +5,23 @@
     <title>Portali i Punes per Studentet</title>
     <meta name="author" content="Aldo Ziflaj">
     <meta name="description" content="Portali i Punes per studente, detyre kursi ne UptWebDesign">
+    <link rel="stylesheet" href="assets/css/base.css">
+    <link rel="stylesheet" href="assets/css/modules.css">
   </head>
   <body>
     <div class="header">
-      Header here
+      <ul class="header-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+        <li>
+          <a href="#">Sign up</a>
+        </li>
+      </ul>
     </div>
