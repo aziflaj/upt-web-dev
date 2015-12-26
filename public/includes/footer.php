5aa1e05861ba7c65a62fb6294a1a9b6c49f4a0d6
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-26 14:43:40 +0100

    fixed footer

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,5 +1,74 @@
-    <div class="footer">
-      Footer here
-    </div>
-  </body>
+<!-- footer -->
+<div class="row row--dark-grey row--padding-medium footer">
+    <footer class="row container-wide">
+        <div class="row">
+            <div class="col-narrow">
+                <div class="footer__success">
+                    <h2 class="footer__section-title">Histori Suksesi</h2>
+                    <h3 class="footer__success--name">Reshat Arbana</h3>
+                    <p class="footer__success--story">
+                        Sot jam zhvillues ne kompanine ABC Software dhe jam i
+                        gëzuar në pozicionin që kam. Kjo falë ndërmjetësimit
+                        të ketij portali.
+                    </p>
+                </div>
+            </div>
+            <div class="col-narrow">
+                <div class="footer__contact">
+                    <h2 class="footer__section-title">Kontakto</h2>
+                    <ul>
+                        <li class="footer__contact-entry">
+                            <a href="mailto:aldoziflaj95@gmail.com">
+                                email@kompania.com
+                            </a>
+                        </li>
+                        <li class="footer__contact-entry">+355 69 324 3548</li>
+                        <li class="footer__contact-entry">+355 68 123 4567</li>
+                        <li class="footer__contact-entry">+355 44 123 456</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-narrow">
+                <div class="footer__help">
+                    <h2 class="footer__section-title">Ndihme</h2>
+                    <ul>
+                        <li class="footer__help-entry">Ndihme per CV</li>
+                        <li class="footer__help-entry">Keshilla interviste</li>
+                        <li class="footer__help-entry">Udhezim karriere</li>
+                        <li class="footer__help-entry">Shembull CV-je</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-narrow">
+                <div class="footer__social">
+                    <h2 class="footer__section-title">Media Sociale</h2>
+                    <ul>
+                        <li class="footer__social-media">
+                            <a href="#">Facebook</a>
+                        </li>
+                        <li class="footer__social-media">
+                            <a href="#">Twitter</a>
+                        </li>
+                        <li class="footer__social-media">
+                            <a href="#">Google+</a>
+                        </li>
+                        <li class="footer__social-media">
+                            <a href="#">LinkedIn</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row container-wide">
+          <div class="col-narrow footer__copyright">
+            &copy; Aldo Ziflaj 2015
+          </div>
+        </div>
+    </footer>
+</div>
+
+
+</body>
 </html>
