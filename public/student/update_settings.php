a6812362c8c4fb7240f8abc838d86951c5580f8c
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-30 13:06:48 +0100

    uploads CV online

--- public/student/update_settings.php
+++ public/student/update_settings.php
@@ -1,29 +1,36 @@
 <?php
-var_dump($_POST);
-if(isset($_FILES['curriculum'])){
-     $errors= array();
-     $file_name = $_FILES['curriculum']['name'];
-     $file_size =$_FILES['curriculum']['size'];
-     $file_tmp =$_FILES['curriculum']['tmp_name'];
-     $file_type=$_FILES['curriculum']['type'];
-     $file_ext=strtolower(end(explode('.',$_FILES['curriculum']['name'])));
+session_start();
 
-     $expensions= array("jpeg","jpg","png", "pdf");
+$db = require_once(__DIR__ . '/../config/db.php');
+$connection = mysqli_connect($db['host'], $db['username'], $db['password'], $db['database']);
 
-     if(in_array($file_ext,$expensions)=== false){
-        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-     }
+$update_sql = "update students ";
+$update_sql .= "set first_name = '{$_POST['first_name']}', ";
+$update_sql .= "last_name = '{$_POST['last_name']}', ";
+$update_sql .= "degree = '{$_POST['education']}' ";
 
-     if($file_size > 2097152){
-        $errors[]='File size must be excately 2 MB';
-     }
+if(isset($_FILES['curriculum'])) {
+  $file_name = $_FILES['curriculum']['name'];
+  $file_arr = explode('.', $file_name);
+  $file_ext = strtolower($file_arr[count($file_arr) - 1]);
 
-     if(empty($errors)==true){
-        move_uploaded_file($file_tmp,"../uploads/".$file_name);
-        echo "Success";
-     }else{
-        print_r($errors);
-     }
-  }
+  $new_filename = "{$_SESSION['user_handle']} {$_SESSION['id']} {$_FILES['curriculum']['tmp_name']} " . time();
+  $new_filename = md5($new_filename) . '.' . $file_ext;
+  move_uploaded_file($_FILES['curriculum']['tmp_name'], __DIR__ . "/../uploads/" . $new_filename);
 
+  $update_sql .= ", cv_url = '/uploads/$new_filename'";
+}
+
+$update_sql .= "where id = {$_SESSION['local_id']}";
+
+if (mysqli_query($connection, $update_sql)) {
+  return header("location: /settings.php");
+} else {
+  include_once("../includes/header.php");
+
+  echo "<h1>Some error occurred</h1>";
+  echo mysqli_error($connection);
+
+  include_once("../includes/footer.php");
+}
 ?>
