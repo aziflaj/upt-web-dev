14742945e1e8f0e905893161746fdabce4e36c81
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-30 16:50:50 +0100

    added a 404 page

---
+++ public/errors/404.php
@@ -0,0 +1,13 @@
+<div class="content error">
+  <div class="error--fourohfour">
+    404
+  </div>
+
+  <div class="error--message">
+    Faqja nuk u gjet
+  </div>
+
+  <div class="error--explanation">
+    Kjo faqe mund te jete levizur diku tjeter, ose linku juaj eshte i gabuar.
+  </div>
+</div>
---
+++ public/user.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once('includes/header.php');
+
+$db = require_once(__DIR__ . '/config/db.php');
+$connection = mysqli_connect($db['host'], $db['username'], $db['password'], $db['database']);
+
+$sql = "select type_id from users where id = {$_GET['id']}";
+$result = mysqli_query($connection, $sql);
+if ($result) {
+  $user = mysqli_fetch_assoc($result); // there should be only 1
+} else {
+  include_once(__DIR__ . "/errors/404.php");
+  include_once('includes/footer.php');
+  return;
+}
+mysqli_close($connection);
+
+if ($user) {
+  $id = $_GET['id'];
+  switch ($user['type_id']) {
+    case 1:
+      include(__DIR__ . "/views/admin/me.php");
+      break;
+
+    case 2:
+      include(__DIR__ . "/views/student/me.php");
+      break;
+
+    case 3:
+      include(__DIR__ . "/views/admin/me.php");
+      break;
+
+    default:
+      include_once(__DIR__ . "/errors/404.php");
+      break;
+  }
+} else {
+  include_once(__DIR__ . "/errors/404.php");
+}
+
+include_once('includes/footer.php');
+?>
